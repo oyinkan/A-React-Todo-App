5f2d53f69fe8da2042d3a44a64fb4af098170f3c
oyinkan <skusman@student.lautech.edu.ng>
2019-10-21 17:40:09 +0100

    made changes to the index page

--- src/App.js
+++ src/App.js
@@ -1,26 +1,48 @@
 import React from 'react';
+import TodoItem from './components/TodoItem';
+import todosData from './components/todosData'
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+class App extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            todos: todosData
+        }
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleChange(id) {
+        this.setState(prevState => {
+            const updatedTodos = prevState.todos.map(todo => {
+                if (todo.id === id) {
+                    return {
+                        ...todo,
+                        completed: !todo.completed
+                    }
+                }
+                return todo
+            })
+            return {
+                todos: updatedTodos
+            }
+        })
+    }
+
+    render() {
+        const todoItems = this.state.todos.map(item => <TodoItem key={item.id} item={item} handleChange={this.handleChange} />)
+        return(
+            <div>
+                <h2 style={{textAlign: 'center'}}>My To-do App</h2>
+                <div className="todo-list">
+                    {todoItems}
+                </div>
+            </div>
+        )
+    }
+} 
+
+
 
 export default App;
